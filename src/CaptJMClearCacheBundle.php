a52f319b175cfef17c556ac84be37cdb6615ef5a
david_makaridze <dmakaridze@gmail.com>
2023-03-09 19:00:09 +0400

    v1.0.9

--- src/CaptJMClearCacheBundle.php
+++ src/CaptJMClearCacheBundle.php
@@ -2,12 +2,14 @@
 
 namespace CaptJM\ClearCacheBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class CaptJMClearCacheBundle extends AbstractBundle
 {
-    public function getPath(): string
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-        return __DIR__;
+        $container->import('Resources/config/services.php');
     }
 }
